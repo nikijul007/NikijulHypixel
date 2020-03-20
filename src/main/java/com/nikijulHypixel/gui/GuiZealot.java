13e03371c5c67fa931498ccdd6c7544c26f0efab
nikijul007 <nikos01@gmx.net>
2020-03-20 16:57:23 +0100

    Gui Text from String Array

--- src/main/java/com/nikijulHypixel/gui/GuiZealot.java
+++ src/main/java/com/nikijulHypixel/gui/GuiZealot.java
@@ -7,6 +7,7 @@ public class GuiZealot extends GuiScreen {
 
 	private int xSize = 176;
 	private int ySize = 166;
+	private int yOffset = 0;
 
 	@Override
 	public void initGui() {
@@ -16,11 +17,14 @@ public class GuiZealot extends GuiScreen {
 	@Override
 	public void drawScreen(int mouseX, int mouseY, float partialTicks) {
 		super.drawScreen(mouseX, mouseY, partialTicks);
-
+		yOffset = 0;
 		this.mc.getTextureManager().bindTexture(new ResourceLocation("nikijulhypixel:textures/gui/background.png"));
 
 		drawTexturedModalRect((this.width - this.xSize) / 2, (this.height - this.ySize) / 2, 0, 0, xSize, ySize);
-		fontRendererObj.drawString("PETER", (this.width - this.xSize) / 2 + 10, (this.height - this.ySize) / 2 + 10, 1);
+		for(String s : getText()) {
+		fontRendererObj.drawString(s, (this.width - this.xSize) / 2 + 10, (this.height - this.ySize) / 2 + 10 + yOffset , 0x000573);
+		yOffset += 15;
+		}
 
 	}
 
@@ -32,6 +36,11 @@ public class GuiZealot extends GuiScreen {
 	@Override
 	public void drawDefaultBackground() {
 		super.drawDefaultBackground();
+	}
+	
+	public String[] getText() {
+		String[] s = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15"};
+		return s;
 	}
 
 }
